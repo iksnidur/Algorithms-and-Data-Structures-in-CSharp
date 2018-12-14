564aa86fec85ae55ea670f56884015db17418908
iksnidur <vesnarudinski@gmail.com>
2018-12-14 11:38:11 +0100

    CarStruct

---
+++ CarStruct/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStruct
+{
+    struct Car
+    {
+        public string Make;
+        public string Model;
+        public string Color;
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Vroom");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Yaris";
+            myCar.Color = "Silver";
+            Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} that is {myCar.Color} in color.");
+            Console.WriteLine();
+            myCar.Accelerate();
+            Console.ReadLine();
+        }
+    }
+}
